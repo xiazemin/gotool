a22a2e5e55be03ac54d5d5b022bd15973b5f8607
xiazemin <465474307@qq.com>
2020-03-25 17:44:42 +0800

    Track 11 files into repository.
    - untracked src/httppprof/exp1/profile
    - modified src/httppprof/exp1/build.sh
    - untracked src/httppprof/exp1/profile.svg
    - untracked src/httppprof/exp2/main.go
    - untracked src/httppprof/exp2/m
    - untracked src/httppprof/exp2/build.sh
    - untracked src/httppprof/exp2/profile
    - untracked src/httppprof/exp2/profile.svg
    - untracked src/httppprof/exp2/heap
    - untracked src/httppprof/exp2/heap.svg
    - untracked src/httppprof/exp2/torch.svg
    
    Auto commit by GitBook Editor

--- src/httppprof/exp1/build.sh
+++ src/httppprof/exp1/build.sh
@@ -4,3 +4,49 @@ wrk -c 400 -t 8 -d 3m http://localhost:9876/test
 -c, --connections <N>  跟服务器建立并保持的TCP连接数量
 -d, --duration    <T>  压测时间
 -t, --threads     <N>  使用多少个线程进行压测
+
+
+$curl -O http://localhost:9876/debug/pprof/profile
+
+$go tool pprof ./m profile
+File: m
+Type: cpu
+Time: Mar 25, 2020 at 4:27pm (CST)
+Duration: 30s, Total samples = 0
+No samples were found with the default sample value type.
+Try "sample_index" command to analyze different sample values.
+Entering interactive mode (type "help" for commands, "o" for options)
+(pprof) top
+Showing nodes accounting for 0, 0% of 0 total
+      flat  flat%   sum%        cum   cum%
+
+
+ $go tool pprof --svg ./m profile > ./profile.svg
+
+
+ $go tool pprof m profile
+ File: m
+ Type: cpu
+ Time: Mar 25, 2020 at 4:54pm (CST)
+ Duration: 30s, Total samples = 10ms (0.033%)
+ Entering interactive mode (type "help" for commands, "o" for options)
+ (pprof) top
+
+
+ Showing nodes accounting for 10ms, 100% of 10ms total
+       flat  flat%   sum%        cum   cum%
+       10ms   100%   100%       10ms   100%  runtime.pthread_cond_wait
+          0     0%   100%       10ms   100%  runtime.findrunnable
+          0     0%   100%       10ms   100%  runtime.mcall
+          0     0%   100%       10ms   100%  runtime.notesleep
+          0     0%   100%       10ms   100%  runtime.park_m
+          0     0%   100%       10ms   100%  runtime.schedule
+          0     0%   100%       10ms   100%  runtime.semasleep
+          0     0%   100%       10ms   100%  runtime.stopm
+
+
+  $go-torch ./m  profile
+  INFO[16:56:43] Run pprof command: go tool pprof -raw -seconds 30 ./m profile
+  INFO[16:56:43] Writing svg to torch.svg
+
+
---
+++ src/httppprof/exp2/build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+go build -o m main.go
+./m
+
+$wrk -c 400 -t 8 -d 3m http://localhost:9876/test
+Running 3m test @ http://localhost:9876/test
+  8 threads and 400 connections
+
+    Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency   702.15ms  510.04ms   2.00s    67.56%
+    Req/Sec    10.31      9.99    80.00     87.38%
+  8383 requests in 3.00m, 160.99MB read
+  Socket errors: connect 0, read 242, write 1, timeout 5208
+Requests/sec:     46.55
+Transfer/sec:      0.89MB
+
+
+$go tool pprof --svg ./m profile > ./profile.svg
+
+$ go tool pprof ./m profile
+File: m
+Type: cpu
+Time: Mar 25, 2020 at 4:39pm (CST)
+Duration: 30.01s, Total samples = 0
+No samples were found with the default sample value type.
+Try "sample_index" command to analyze different sample values.
+Entering interactive mode (type "help" for commands, "o" for options)
+(pprof) top
+Showing nodes accounting for 0, 0% of 0 total
+      flat  flat%   sum%        cum   cum%
+(pprof) exit
+
+
+$go-torch ./m  profile
+INFO[16:43:37] Run pprof command: go tool pprof -raw -seconds 30 ./m profile
+ERROR: No stack counts found
+FATAL[16:43:37] Failed: could not generate flame graph: exit status 2
+
+$curl -O  http://localhost:9876/debug/pprof/heap
+
+$go tool pprof m heap
+File: m
+Type: inuse_space
+Time: Mar 25, 2020 at 4:45pm (CST)
+Entering interactive mode (type "help" for commands, "o" for options)
+(pprof) top
+Showing nodes accounting for 1.50MB, 100% of 1.50MB total
+      flat  flat%   sum%        cum   cum%
+    1.50MB   100%   100%     1.50MB   100%  runtime.malg
+         0     0%   100%     1.50MB   100%  runtime.mstart
+         0     0%   100%     1.50MB   100%  runtime.newproc.func1
+         0     0%   100%     1.50MB   100%  runtime.newproc1
+         0     0%   100%     1.50MB   100%  runtime.systemstack
+(pprof)
+
+$go tool pprof --svg m heap > heap.svg
+
+
+
+
