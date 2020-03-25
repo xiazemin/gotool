f99f3c83123563021acc8356c069ac067e13e809
xiazemin <465474307@qq.com>
2020-03-25 12:59:30 +0800

    Track 3 files into repository.
    - untracked src/httppprof/exp1/main.go
    - untracked src/httppprof/exp1/m
    - untracked src/httppprof/exp1/build.sh
    
    Auto commit by GitBook Editor

---
+++ src/httppprof/exp1/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+brew install wrk
+wrk -c 400 -t 8 -d 3m http://localhost:9876/test
+-c, --connections <N>  跟服务器建立并保持的TCP连接数量
+-d, --duration    <T>  压测时间           
+-t, --threads     <N>  使用多少个线程进行压测
